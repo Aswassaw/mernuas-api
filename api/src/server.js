e632a105e4378dab6dcb1fdbf782acc56c8aa4c4
Andry Pebrianto <andrypeb227@gmail.com>
2021-10-12 18:58:21 +0700

    Menggunakan package yang membantu seperti cors, morgan, compression, helmet, dotenv, dll dsb

--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,29 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const chalk = require("chalk");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Server Running");
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(compression());
+app.use(helmet());
+
+// Root Endpoint
+app.get("/", (req, res) => {
+  res.send("MERN Ultimate Auth");
 });
 
-app.listen(5000, () => {
-  console.log("Running");
+// Running Server
+const PORT = process.env.API_PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+  console.log(chalk`Visit {rgb(128, 237, 153) http://localhost:${PORT}}`);
+  console.log(chalk`Developed by {rgb(255, 92, 88) Andry Pebrianto}`);
 });
