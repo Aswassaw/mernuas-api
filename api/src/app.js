394f24929690158848a51ea828546a0ac7c8fe79
Andry Pebrianto <andrypeb227@gmail.com>
2021-12-13 23:16:59 +0700

    Create landing page for api

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const fs = require("fs");
 const cors = require("cors");
 const morgan = require("morgan");
 const compression = require("compression");
@@ -15,7 +16,11 @@ connectToDB();
 // Middlewares
 app.use(express.json());
 app.use(compression());
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 if (process.env.NODE_ENV === "production") {
   app.use(morgan("combined"));
   app.use(cors({ origin: "https://mernuas.netlify.app" }));
@@ -26,7 +31,19 @@ if (process.env.NODE_ENV === "production") {
 
 // Root Endpoint
 app.get("/", (req, res) => {
-  res.send("Mernuas - MERN Ultimate Auth System");
+  fs.readFile(__dirname + "/v1/html/index.html", (err, data) => {
+    res.set("Content-Type", "text/html");
+    // Jika file tidak ditemukan
+    if (err) {
+      res.write("<title>Mernuas</title>");
+      res.write("<h1>Mernuas - MERN Ultimate Auth System</h1>");
+      res.write(`<p>Read API Documentation: <a href="#">Doc</a></p>`);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 // Endpoint (v1)
@@ -40,7 +57,9 @@ app.use("/", (req, res) => {
 // Running Server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT} with ${process.env.NODE_ENV} environment`);
+  console.log(
+    `Server started on port ${PORT} with ${process.env.NODE_ENV} environment`
+  );
   console.log(chalk`Visit {rgb(128, 237, 153) http://localhost:${PORT}}`);
   console.log(chalk`Developed by {rgb(255, 92, 88) Andry Pebrianto}`);
 });
