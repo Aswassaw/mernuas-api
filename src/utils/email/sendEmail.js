28247a8a5f0795206afd0d8db53ebf4e8b776bbb
Andry Pebrianto <andrypeb227@gmail.com>
2021-12-18 15:11:22 +0700

    Mengubah metode pengiriman email dari gmail biasa menjadi OAuth2 Goole Cloud

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -1,7 +1,35 @@
-const transporter = require("../../config/email");
+require("dotenv").config();
+const { google } = require("googleapis");
+const nodemailer = require("nodemailer");
+const smtpPool = require("nodemailer-smtp-pool");
 
-const sendEmail = (dataEmail) => {
-  return transporter
+// oayh2 config
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+
+const sendEmail = async (dataEmail) => {
+  // config nodemailer
+  const accessToken = await oAuth2Client.getAccessToken();
+  const transporter = nodemailer.createTransport(
+    {
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL_USER,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken,
+      }
+    }
+  );
+
+  // send email
+  transporter
     .sendMail(dataEmail)
     .then((info) => {
       console.log("Email sended successfully.");
