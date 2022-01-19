60a3e647b3b91c7f5aa567d804d4a92202d5bd1f
Andry Pebrianto <andrypeb227@gmail.com>
2022-01-19 21:07:21 +0700

    Mengubah kesalahan penulisan dari CLIENT_ID menjadi GOOGLE_CLIENT_ID

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -4,40 +4,43 @@ const nodemailer = require("nodemailer");
 
 // oayh2 config
 const oAuth2Client = new google.auth.OAuth2(
-  process.env.CLIENT_ID,
+  process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.REDIRECT_URL
+  process.env.REDIRECT_URI
 );
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 const sendEmail = async (dataEmail) => {
-  // config nodemailer
-  const accessToken = await oAuth2Client.getAccessToken();
-  const transporter = nodemailer.createTransport(
-    {
+  try {
+    // config nodemailer
+    const accessToken = await oAuth2Client.getAccessToken();
+    const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         type: "OAuth2",
         user: process.env.EMAIL_USER,
-        clientId: process.env.CLIENT_ID,
+        clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         refreshToken: process.env.REFRESH_TOKEN,
         accessToken: accessToken,
-      }
-    }
-  );
-
-  // send email
-  transporter
-    .sendMail(dataEmail)
-    .then((info) => {
-      console.log("Email sended successfully.");
-      console.log(info);
-    })
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
+      },
     });
+
+    // send email
+    transporter
+      .sendMail(dataEmail)
+      .then((info) => {
+        console.log("Email sended successfully.");
+        console.log(info);
+      })
+      .catch((error) => {
+        console.error(error);
+        process.exit(1);
+      });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
 
 module.exports = sendEmail;
